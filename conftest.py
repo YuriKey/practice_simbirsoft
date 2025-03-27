098c13994ef9dd95f282a223a9179867c6c502f1
YuriKey <krenev_yu@mail.ru>
2025-03-27 16:56:20 +0400

    ci/cd test 5

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+"""Local launch"""
+
 # from selenium import webdriver
 # from selenium.webdriver.chrome.options import Options
 # import pytest
@@ -16,20 +18,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 
+"""CI/CD launch"""
 @pytest.fixture(scope="function")
 def browser():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--remote-debugging-port=9222")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--disable-gpu")
 
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     yield driver
     driver.quit()
