2c1bd92e4bd35811fa1f6fd5a76b4cbd8b16150e
YuriKey <krenev_yu@mail.ru>
2025-03-27 17:21:55 +0400

    ci/cd test 7

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
+
 """Local launch"""
 
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# import pytest
-#
-#
+
 # @pytest.fixture(scope='function')
 # def browser():
 #     chrome_options = Options()
@@ -14,18 +15,7 @@
 #     yield chrome_browser
 #     chrome_browser.quit()
 
-
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.options import Options
-# import pytest
-
-
 """CI/CD launch"""
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import pytest
 
 
 @pytest.fixture(scope='function')
@@ -39,29 +29,3 @@ def browser():
     chrome_browser.implicitly_wait(3)
     yield chrome_browser
     chrome_browser.quit()
-
-
-
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.options import Options
-# import pytest
-#
-#
-# @pytest.fixture(scope="function")
-# def browser():
-#     chrome_options = Options()
-#     chrome_options.add_argument("--headless=new")  # Используем новый headless режим
-#     chrome_options.add_argument("--no-sandbox")
-#     chrome_options.add_argument("--disable-dev-shm-usage")
-#     chrome_options.add_argument("--remote-debugging-port=9222")
-#     chrome_options.add_argument("--disable-gpu")
-#     chrome_options.add_argument("--window-size=1920,1080")
-#
-#     service = Service(ChromeDriverManager().install())
-#     driver = webdriver.Chrome(service=service, options=chrome_options)
-#     driver.implicitly_wait(10)  # Добавляем неявное ожидание
-#
-#     yield driver
-#     driver.quit()
